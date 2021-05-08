4bfd17eb88f0ba127ad744cca56de653649bc5fd
Extrabution <super.vanya-inchin-2002@ya.ru>
2021-05-08 06:38:00 +0300

    Working but not right

--- rts_game.cpp
+++ rts_game.cpp
@@ -27,7 +27,18 @@ public:
             return true;
         else return false;
     }
+    bool operator != (Position right) const{
+        auto[il, jl] = pos;
+        auto[ir, jr] = right.pos;
+        if(il != ir || jl != jr)
+            return true;
+        else return false;
+    }
 };
+
+Position currentLeader;
+Position nextMove;
+bool smallFlag = false;
 
 class Unit{
 public:
@@ -46,7 +57,6 @@ public:
     vector<shared_ptr<Unit>> player1;
     vector<shared_ptr<Unit>> mountains;
     vector<vector<shared_ptr<Unit>>> fieldPTR;
-    Position currentLeader;
     World() {
         for (int i = 0; i < 15; i++) {
             vector<shared_ptr<Unit>> temp;
@@ -67,7 +77,7 @@ public:
                     player0.push_back(temp);
                     player0.back()->position.pos = {i,j};
                     field[i + 14][j + 8] = "R";
-                    shared_ptr<Unit> temp1(new Unit("r", "S"));
+                    shared_ptr<Unit> temp1(new Unit("R", "s"));
                     fieldPTR[i + 14][j+8] = temp1;
                     player1.push_back(temp1);
                     player1.back()->position.pos = {i+14,j+8};
@@ -78,7 +88,7 @@ public:
                     player0.push_back(temp);
                     player0.back()->position.pos = {i,j};
                     field[i + 8][j + 8] = "R";
-                    shared_ptr<Unit> temp1(new Unit("r", "S"));
+                    shared_ptr<Unit> temp1(new Unit("R", "s"));
                     fieldPTR[i + 8][j+8] = temp1;
                     player1.push_back(temp1);
                     player1.back()->position.pos = {i+8,j+8};
@@ -89,7 +99,7 @@ public:
                     player0.push_back(temp);
                     player0.back()->position.pos = {i,j};
                     field[i + 12][j + 8] = "P";
-                    shared_ptr<Unit> temp1(new Unit("p", "R"));
+                    shared_ptr<Unit> temp1(new Unit("P", "r"));
                     fieldPTR[i + 12][j+8] = temp1;
                     player1.push_back(temp1);
                     player1.back()->position.pos = {i+12,j+8};
@@ -100,7 +110,7 @@ public:
                     player0.push_back(temp);
                     player0.back()->position.pos = {i,j};
                     field[i + 6][j + 8] = "P";
-                    shared_ptr<Unit> temp1(new Unit("P", "R"));
+                    shared_ptr<Unit> temp1(new Unit("P", "r"));
                     fieldPTR[i + 6][j+8] = temp1;
                     player1.push_back(temp1);
                     player1.back()->position.pos = {i+6,j+8};
@@ -111,7 +121,7 @@ public:
                     player0.push_back(temp);
                     player0.back()->position.pos = {i,j};
                     field[i + 10][j + 8] = "S";
-                    shared_ptr<Unit> temp1(new Unit("r", "S"));
+                    shared_ptr<Unit> temp1(new Unit("S", "p"));
                     fieldPTR[i + 10][j+8] = temp1;
                     player1.push_back(temp1);
                     player1.back()->position.pos = {i+10,j+8};
@@ -133,7 +143,7 @@ public:
         player0.back()->position.pos = {0,0};
         player1.push_back(std::make_shared<Unit>("F", ""));
         player1.back()->position.pos = {14,14};
-        currentLeader = fieldPTR[13][9]->position;
+        currentLeader = fieldPTR[13][9]->position.pos;
         //Vector of Mountains coordinates for easier implementation;
         vector<tuple<int,int>> MPosition = {{2,10},{2,12},{4,9},{4,13},
                                             {5,10},{5,11},{5,12},{7,6},
@@ -176,53 +186,43 @@ Action actionPlayerZero(const World world) {
         shared_ptr<Unit> unit = world.player0[rand() % world.player0.size()];
         auto[i,j] = unit->position.pos;
         Action action;
-        if(world.field[i][j] == "f")
-            continue;
-        else if(position == 0 && j - 1 != -1 && (world.field[i][j-1] == " " || world.field[i][j-1] == unit->canKill)) {
+        if(position == 0 && j - 1 != -1 && (world.field[i][j-1] == " " || world.field[i][j-1] == unit->canKill)) {
             action.from = unit->position.pos;
             action.to = {i, j - 1};
-            unit->killed = true;
             return action;
         }
         else if(position == 1 && i -1 != -1 && (world.field[i-1][j] == " " || world.field[i-1][j] == unit->canKill)){
             action.from = unit->position.pos;
             action.to = {i - 1, j};
-            unit->killed = true;
             return action;
         }
         else if(position == 2 && j + 1 != 15 && (world.field[i][j+1] == " " || world.field[i][j+1] == unit->canKill)){
             action.from = unit->position.pos;
             action.to = {i, j + 1};
-            unit->killed = true;
             return action;
         }
         else if(position == 3 && i + 1 != 15 && (world.field[i+1][j] == " " || world.field[i+1][j] == unit->canKill)){
             action.from = unit->position.pos;
             action.to = {i + 1, j};
-            unit->killed = true;
             return action;
         }
         else if(j + 1!= 15 &&(world.field[i][j+1] == " " || world.field[i][j+1] == unit->canKill)){ // right
             action.from = unit->position.pos;
             action.to = {i, j + 1};
-            unit->killed = true;
             return action;
         }
         else if(j - 1 != -1 && (world.field[i][j-1] == " " || world.field[i][j-1] == unit->canKill )){ // left
             action.from = unit->position.pos;
             action.to = {i, j - 1};
-            unit->killed = true;
             return action;
         }
         else if(i + 1 != 15 && (world.field[i+1][j] == " " || world.field[i+1][j] == unit->canKill )){ // down
             action.from = unit->position.pos;
             action.to = {i + 1, j};
-            unit->killed = true;
             return action;
         }
         else if(i - 1 != -1 && (world.field[i-1][j] == " " || world.field[i-1][j] == unit->canKill )){ // up
             action.from = unit->position.pos;
-            unit->killed = true;
             action.to = {i - 1, j };
             return action;
         }
@@ -231,14 +231,42 @@ Action actionPlayerZero(const World world) {
 
 Action actionPlayerOne(const World world) {
     // TODO Implement some basic strategy and remove the lines below
-    auto[i,j] = world.currentLeader.pos;
+    Action action;
+    auto[i,j] = currentLeader.pos;
     Position temp;
     temp = {-1,-1};
-    if(world.currentLeader == temp){
-
+    if(currentLeader == temp){
+        currentLeader = world.player1.front()->position.pos;
     }
-    else{
-        world.currentLeader.pos = world.player1.back()->position.pos;
+    cout<<251;
+    if(world.field[i-1][j] == "f"){
+        cout<<1;
+        smallFlag = true;
+        action.from = currentLeader.pos;
+        currentLeader = {i-1, j};
+        action.to = {i-1, j};
+        return action;
+    }
+    if(j - 1 != -1 && (world.field[i][j-1] == " " || world.field[i][j-1] == world.fieldPTR[i][j]->canKill )){
+        cout<<2;
+        action.from = currentLeader.pos;
+        currentLeader = {i, j-1};
+        action.to = {i, j-1};
+        cout<<7;
+        return action;
+    }
+    else if(j - 1 == -1 || world.field[i][j-1] == "M"){
+        cout<<3;
+        action.from = currentLeader.pos;
+        currentLeader = {i-1, j};
+        action.to = {i-1, j};
+        return action;
+    }
+    else if(world.field[i-1][j] != " "){
+        action.from = currentLeader.pos;
+        currentLeader = {i,j+1};
+        action.to = {i,j+1};
+        return action;
     }
     std::cout << "actionPlayerOne()" << endl;
     std::this_thread::sleep_for(385ms); // 0.4 seconds
@@ -262,11 +290,12 @@ std::tuple<Action, bool> waitPlayer(Action (*f)(World), World world) {
 
 int main() {
     World world;
+    world.print();
     bool endGame = false;
     int ik = 0;
-    while (ik !=1000) {
+    while (!endGame) {
         string d;
-        cin>>d;
+        //cin>>d;
         ik++;
         cout<<endl<<ik<<endl;
         auto[action0, timeout0] = waitPlayer(actionPlayerZero, world);
@@ -275,20 +304,32 @@ int main() {
         auto[iTo0,jTo0] = action0.to.pos;
         auto unit = world.fieldPTR[iFrom0][jFrom0];
         cout<<259;
-        if(unit->killed){
-            world.field[iTo0][jTo0] = " ";
-            world.fieldPTR[iTo0][jTo0] = nullptr;
-            Position temp;
-            int i = 0;
-            for(auto it = world.player1.begin(); it != world.player1.end(); it++, i++) {
-                if (world.player1[i]->position == action0.to){
-                    world.player1.erase(it);
-                    break;
-                }
+        int i = 0;
+        for(auto it = world.player1.begin(); it != world.player1.end(); it++, i++) {
+            if (world.player1[i]->position == action0.to) {
+                if (world.player1[i]->position == currentLeader)
+                    currentLeader = {-1, -1};
+                world.player1.erase(it);
+                world.field[iTo0][jTo0] = " ";
+                world.fieldPTR[iTo0][jTo0] = nullptr;
+                break;
             }
         }
         cout<<278;
         auto[action1, timeout1] = waitPlayer(actionPlayerOne, world);
+        auto[iFrom1,jFrom1] = action1.from.pos;
+        auto[iTo1,jTo1] = action1.to.pos;
+        unit = world.fieldPTR[iFrom1][jFrom1];
+        cout<<259;
+        i = 0;
+        for(auto it = world.player0.begin(); it != world.player0.end(); it++, i++) {
+            if (world.player0[i]->position == action1.to){
+                world.player0.erase(it);
+                world.field[iTo1][jTo1] = " ";
+                world.fieldPTR[iTo1][jTo1] = nullptr;
+                break;
+            }
+        }
         if (timeout0 || timeout1) {
             endGame = true;
             if(timeout0)
@@ -296,23 +337,39 @@ int main() {
             else cout<<endl<<"PLAYER 1 TIMED OUT!"<<endl;
             world.print();
         } else {
-            // TODO Validate the actions: actionPlayer0 and actionPlayer1
-
-            // TODO Update the world
             cout<<282;
             world.field[iTo0][jTo0] = world.field[iFrom0][jFrom0];
             world.field[iFrom0][jFrom0] = " ";
+
+            world.field[iTo1][jTo1] = world.field[iFrom1][jFrom1];
+            world.field[iFrom1][jFrom1] = " ";
+
             int i = 0;
             for(auto it = world.player0.begin(); it != world.player0.end(); it++, i++){
-                if( world.player0[i]->position == action0.from)
+                if( world.player0[i]->position == action0.from) {
                     world.player0[i]->position = action0.to;
+                    break;
+                }
             }
             world.fieldPTR[iFrom0][jFrom0]->position = action0.to;
             world.fieldPTR[iTo0][jTo0] = world.fieldPTR[iFrom0][jFrom0];
             world.fieldPTR[iFrom0][jFrom0] = nullptr;
-            // TODO Show the world
+
+            i = 0;
+            for(auto it = world.player1.begin(); it != world.player1.end(); it++, i++){
+                if( world.player1[i]->position == action1.from) {
+                    world.player1[i]->position = action1.to;
+                    break;
+                }
+            }
+            world.fieldPTR[iFrom1][jFrom1]->position = action1.to;
+            world.fieldPTR[iTo1][jTo1] = world.fieldPTR[iFrom1][jFrom1];
+            world.fieldPTR[iFrom1][jFrom1] = nullptr;
+            if(smallFlag) {
+                endGame = true;
+                cout << endl << "PLAYER 1 CAPTURED THE FLAG!" << endl;
+            }
             world.print();
-            // TODO End the game and show the result if a player won
         }
     }
     return 0;
